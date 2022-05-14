00782b579eda2c4aae9bf94f6637c003c5b1c3ae
Gaurav Varshikar <gaurav.varshikar@gmail.com>
2022-05-15 00:13:14 +0530

    Write a program to sort a stack in ascending order.

---
+++ src/StacksAndQueues/SortedStackQuestion.java
@@ -0,0 +1,68 @@
+package StacksAndQueues;
+
+import java.util.LinkedList;
+
+// Write a program to sort a stack in ascending order. You may use additional stacks to hold items,
+// but you may not copy the elements into any other data structures such as an array.
+// The stack supports the following operations such as: push, pop, peek and isEmpty
+public class SortedStackQuestion {
+    public static void main(String[] args) {
+        SortedStack sortedStack = new SortedStack();
+        sortedStack.push(2); sortedStack.print();
+        sortedStack.push(4); sortedStack.print();
+        sortedStack.push(1); sortedStack.print();
+        sortedStack.push(3); sortedStack.print();
+
+        System.out.println(sortedStack.isEmpty());
+        System.out.println(sortedStack.peek());
+        System.out.println(sortedStack.pop());
+        System.out.println(sortedStack.peek());
+        System.out.println(sortedStack.pop());
+        System.out.println(sortedStack.peek());
+        System.out.println(sortedStack.pop());
+        System.out.println(sortedStack.peek());
+        System.out.println(sortedStack.pop());
+        System.out.println(sortedStack.peek());
+    }
+}
+
+class SortedStack {
+    private LinkedList<Integer> stack = new LinkedList<>();
+
+    public void push(int value) {
+        if(stack.isEmpty()){
+            stack.push(value);
+        }
+        else if(value < stack.getFirst()){
+            stack.add(0, value);
+        }
+        else if(value > stack.getLast()){
+            stack.add(stack.size(), value);
+        }
+        else {
+            for(int index = 1; index < stack.size(); index++){
+                if(value > stack.get(index-1) && value < stack.get(index)) {
+                    stack.add(index, value);
+                }
+            }
+        }
+    }
+
+    public int pop(){
+        if(!stack.isEmpty()) return stack.pop();
+        else return -1;
+    }
+
+    public int peek() {
+        if(!stack.isEmpty()) return stack.peek();
+        else return -1;
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public void print() {
+        System.out.println(stack);
+    }
+}
